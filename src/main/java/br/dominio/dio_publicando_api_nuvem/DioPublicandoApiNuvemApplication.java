b941f8e1c0d83a2db9307775ab49716cc7028435
Fagner Cruz <fagner@sti.ufpb.br>
2025-04-21 21:30:16 -0300

    Initial commit

---
+++ src/main/java/br/dominio/dio_publicando_api_nuvem/DioPublicandoApiNuvemApplication.java
@@ -0,0 +1,13 @@
+package br.dominio.dio_publicando_api_nuvem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioPublicandoApiNuvemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioPublicandoApiNuvemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dominio/dio_publicando_api_nuvem/DioPublicandoApiNuvemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dominio.dio_publicando_api_nuvem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioPublicandoApiNuvemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
