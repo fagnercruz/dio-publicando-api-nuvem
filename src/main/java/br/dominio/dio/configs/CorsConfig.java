7d9b79582b8ac90a9cc1bbfa6af81f625fd127f1
Fagner Cruz <fagner@sti.ufpb.br>
2025-04-26 21:48:52 -0300

    21ª Tentando adicionar o Swagger sem quebrar a aplicação - add CorsConfig

---
+++ src/main/java/br/dominio/dio/configs/CorsConfig.java
@@ -0,0 +1,23 @@
+package br.dominio.dio.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://restapi-production-bradesco.up.railway.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
